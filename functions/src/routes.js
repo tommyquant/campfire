99ee3f25ed5196580a29345ac18b92f31be35a8e
Tommy <tommyquant@gmail.com>
2019-06-09 08:24:42 +1000

    Changed name of global variables from 'common' to 'global'

--- functions/src/routes.js
+++ functions/src/routes.js
@@ -18,23 +18,10 @@ app.use(function (req, res, next) {
     return next();
 });
 
-// Easy way to map environments to a different name in case the name is
-// different in the remote config.
-const envMappings = {
-    'development': 'dev',
-    'production': 'prod'
-};
-
 // Get config for specified app and environment
 app.get('/:app/:environment', async function (req, res) {
-    // Check if environment is valid
-    const env = envMappings[req.params.environment];
-    if (env === undefined) {
-        return res.boom.badRequest('Undefined environment');
-    }
-
     try {
-        const data = await config.getConfig(req.params.app, env);
+        const data = await config.getConfig(req.params.app, req.params.environment);
         return res.send(data);
     } catch (error) {
         return res.boom.badImplementation();
